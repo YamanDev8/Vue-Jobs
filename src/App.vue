f582d4d55d5a5ddfa69ecef0a5e6bc51834b8fb7
Yaman <yaman.ghannoum@yahoo.com>
2025-01-18 13:30:08 +0100

    feat(App.vue): add toggle status method and link to Google

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,17 @@ export default {
       name: "Yaman",
       status: false,
       tasks: ["task1", "task2", "task3"],
+      link: "https://google.com",
     };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status) {
+        this.status = false;
+      } else if (!this.status) {
+        this.status = true;
+      }
+    },
   },
 };
 </script>
@@ -16,6 +26,9 @@ export default {
   <p v-if="status">user is active</p>
   <p v-else>user is inactive</p>
   <ul>
-    <li v-for="task in tasks" :key="task"></li>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
+  <a :href="link">Go to google</a>
+  <br />
+  <button v-on:click="toggleStatus">Toggle status</button>
 </template>
